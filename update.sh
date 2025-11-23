944be8e48afd459253f33f28c23d91d174ecfa9a
Nick Pisani <napisani@yahoo.com>
2025-11-22 19:55:13 -0500

    update script

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+TAG="$1"
+if [ -z "$TAG" ]; then
+	echo "Usage: $0 <tag>"
+	exit 1
+fi
+
+mkdir -p tmp
+TRAP_EXIT() {
+	popd
+}
+pushd ./tmp
+
+wget "https://github.com/napisani/proctmux/releases/download/$TAG/proctmux-darwin-arm64.tar.gz"
+wget "https://github.com/napisani/proctmux/releases/download/$TAG/proctmux-darwin-amd64.tar.gz"
+
+# Calculate SHA256 checksums
+echo "ARM64 SHA256:"
+ARM=$(shasum -a 256 proctmux-darwin-arm64.tar.gz | awk '{print $1}')
+echo "$ARM"
+
+echo "AMD64 SHA256:"
+AMD=$(shasum -a 256 proctmux-darwin-amd64.tar.gz | awk '{print $1}')
+echo "$AMD"
+
+cat ../Formula/proctmux.rbtemplate | sed "s/ARM64_SHA256_PLACEHOLDER/$ARM/" | sed "s/AMD64_SHA256_PLACEHOLDER/$AMD/" >../Formula/proctmux.rb
+rm -rf ./*
